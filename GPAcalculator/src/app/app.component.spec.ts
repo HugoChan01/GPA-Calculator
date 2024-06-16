84936958a2322f81b5992a3ddaacd42ce51aa291
HugoChan01 <hugo2001421@gmail.com>
2024-06-16 22:57:00 +0800

    still try

--- GPAcalculator/src/app/app.component.spec.ts
+++ GPAcalculator/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'GPAcalculator' title`, () => {
+  it(`should have the 'gpacalculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GPAcalculator');
+    expect(app.title).toEqual('gpacalculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, GPAcalculator');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gpacalculator');
   });
 });
